68e066ffea5a178ae691141075ebdace3600b1f0
Ajumobi12 <117199684+Ajumobi12@users.noreply.github.com>
2022-11-05 13:00:27 +0100

    Update Natural Number code.c

--- Natural Number code.c
+++ Natural Number code.c
@@ -1,20 +1,46 @@
-#include<stdio.h>
+
+
+/**
+ * C program to print all natural numbers from 1 to n
+ */
+
+#include <stdio.h>
 
 int main()
+{
+    int i, n;
+
+    /* Input upper limit from user */
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    printf("Natural numbers from 1 to %d : \n", n);
+
+    /*
+     * Start loop counter from 1 (i=1) and go till n (i<=n)
+     * increment the loop count by 1 to get the next value. 
+     * For each repetition print the value of i.
+     */
+    for(i=1; i<=n; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
 
 {
-     int num, count;
 
-printf("Enter a positive number\n");
-scanf("%d", &num);
 
-printf("\nNatural numbers from 1 to %d are:\n", num);
 
-for(count = 1; count <= num; count++)
-{
-	printf("%d\t", count);
-printf("\n");
 
-return 0;
-}
-}
+
+
+
+
+
+
+
+
+
+
